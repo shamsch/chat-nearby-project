a3fb8d3f548141f06b79e41fb985a567a84ee377
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-01 15:52:47 +0300

    to fix: check db for user location when re start

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,20 @@ function App() {
 	const chat = useStore((state) => state.chat);
 
 	return (
-			<Routes>
-				<Route
-					path="/"
-					element={chat ? <Navigate to="/chat" /> : <Navigate to="/start" />}
-				/>
-				<Route
-					path="/start"
-					element={chat ? <Navigate to="/chat" /> : <StartChatPage />}
-				/>
-				<Route
-					path="/chat"
-					element={chat ? <ChatPage /> : <Navigate to="/start" />}
-				/>
-			</Routes>
+		<Routes>
+			<Route
+				path="/"
+				element={chat ? <Navigate to="/chat" /> : <Navigate to="/start" />}
+			/>
+			<Route
+				path="/start"
+				element={chat ? <Navigate to="/chat" /> : <StartChatPage />}
+			/>
+			<Route
+				path="/chat"
+				element={chat ? <ChatPage /> : <Navigate to="/start" />}
+			/>
+		</Routes>
 	);
 }
 
