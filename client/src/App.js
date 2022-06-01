fafbd50832d15b409305e3f4412a4330c57c569f
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-01 15:10:05 +0300

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,152 +1,27 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import { Context } from "./context/store";
 import { useStore } from "./zustand/zustandStore";
-import shallow from 'zustand/shallow'
+import { Navigate, Routes, Route } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import { StartChatPage } from "./pages/StartChatPage";
 
 function App() {
-    // const [chat, setChat] = useState(false);
-    // const [allMessage, setAllMessage] = useState([]);
-    const [allMessage, setAllMessage] = useStore(state => [state.allMessage, state.setAllMessage], shallow) 
-    const [chat, setChat] = useStore(state => [state.chat, state.setChat], shallow) 
-    const [msg, setMsg] = useState("");
-    const [chatRoom, setChatRoom] = useState(null);
-    const [selfID, setSelfID] = useState(null);
-    const [secondUser, setSecondUser] = useState(null);
-    const [isTyping, setIsTyping] = useState(false);
-    const [chatAlive, setChatAlive] = useState(true);
-    const [userCount, setUserCount] = useState(0);
-    const isTypingRef = useRef();
-    const selfIDRef = useRef();
-    const secondUserRef = useRef();
-    const socket = useContext(Context);
+	const chat = useStore((state) => state.chat);
 
-    const joinChat = async () => {
-        if ("geolocation" in navigator) {
-            await navigator.geolocation.getCurrentPosition(async (pos) => {
-                const location = {
-                    x: pos.coords.longitude,
-                    y: pos.coords.latitude,
-                };
-                await socket.emit("create_chat", location);
-                setChat(true);
-            });
-        } else {
-            alert("You location is not available");
-        }
-    };
-
-    useEffect(() => {
-        socket.on("recieve_message", (data) => {
-            console.log("got message:", data);
-            setAllMessage(data);
-
-            //on getting a message 
-            setIsTyping(false);
-            isTypingRef.current=false; 
-        });
-
-        socket.on("chat_room", (data) => {
-            setChatRoom(data.room);
-            setSelfID(data.socketID);
-            selfIDRef.current = data.socketID;
-        });
-
-        socket.on("other_typing", (data) => {
-            console.log("got run", data.user !== selfIDRef.current);
-            
-            if (data.user !== selfIDRef.current) {
-                
-                if (data.typing && !isTypingRef.current) {
-                    setIsTyping(true);
-                    isTypingRef.current = true;
-                } else if (!data.typing && isTypingRef.current) {
-                    setIsTyping(false);
-                    isTypingRef.current = false;
-                }
-            }
-        });
-
-        socket.on("user_disconnect", (data) => {
-            if (data === secondUserRef.current) {
-                setChatAlive(false);
-            }
-        });
-
-        socket.on("user_count", (data) => {
-            setUserCount(data);
-        });
-    }, [socket, setAllMessage]);
-
-    useEffect(() => {
-        //if first user, listen for second user
-        if (chatRoom && selfID && chatRoom === selfID) {
-            socket.on("2nd_user", (data) => {
-                setSecondUser(data);
-                secondUserRef.current = data;
-            });
-        }
-        //else check, if you are the second user yourself in which case your second user is the room which is first user
-        else if (chatRoom && selfID && chatRoom !== selfID) {
-            setSecondUser(chatRoom);
-            secondUserRef.current = chatRoom;
-        }
-    }, [chatRoom, socket, selfID]);
-
-    const sendMessage = async () => {
-        const data = { message: msg, room: chatRoom, owner: selfID };
-        await socket.emit("message_send", data);
-        setMsg("");
-    };
-
-    const handleTyping = (e) => {
-        setMsg(e.target.value)
-        const data = {
-            room: chatRoom,
-            user: selfID,
-            typing: e.target.value.length > 0,
-        };
-        socket.emit("self_typing", data);
-    };
-
-    console.log(chat)
-    if (chat) {
-        return (
-            <div>
-                <h1>chat room: {chatRoom}</h1>
-                {userCount ? <p>People online {userCount}</p> : null}
-                {secondUser ? null : <p>Waiting for user...</p>}
-                {chatAlive ? null : <p>user disconnected</p>}
-                {isTyping ? <p>Other user is typing ... </p> : null}
-                <input
-                    type="text"
-                    value={msg}
-                    onChange={(e) => handleTyping(e)}
-                />
-                <button onClick={sendMessage}>send message</button>
-                {allMessage.map((content, index) => {
-                    if (content.owner === selfID) {
-                        return (
-                            <p key={index} style={{ background: "green" }}>
-                                {content.message}
-                            </p>
-                        );
-                    } else {
-                        return (
-                            <p key={index} style={{ background: "red" }}>
-                                {content.message}
-                            </p>
-                        );
-                    }
-                })}
-            </div>
-        );
-    }
-    return (
-        <div>
-            {userCount ? <p>People online {userCount}</p> : null}
-            <button onClick={joinChat}>Join chat</button>
-        </div>
-    );
+	return (
+			<Routes>
+				<Route
+					path="/"
+					element={chat ? <Navigate to="/chat" /> : <Navigate to="/start" />}
+				/>
+				<Route
+					path="/start"
+					element={chat ? <Navigate to="/chat" /> : <StartChatPage />}
+				/>
+				<Route
+					path="/chat"
+					element={chat ? <ChatPage /> : <Navigate to="/start" />}
+				/>
+			</Routes>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Context, socket } from "./context/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Context.Provider value={socket}>
-        <App />
-    </Context.Provider>
+	<BrowserRouter>
+		<Context.Provider value={socket}>
+			<App />
+		</Context.Provider>
+	</BrowserRouter>
 );
