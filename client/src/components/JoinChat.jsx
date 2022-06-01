ddff94f43cfcd283c8e181ab7aa75ae66046f705
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-01 15:09:59 +0300

    refactored into component

--- client/src/components/JoinChat.jsx
+++ client/src/components/JoinChat.jsx
@@ -1,8 +1,31 @@
+import React, { useContext } from "react";
+import { useStore } from "../zustand/zustandStore";
+import { Context } from "../context/store";
 
-const JoinChat = () => {
-  return (
-    <div>JoinChat</div>
-  )
-}
+export const JoinChat = () => {
+	const userCount = useStore((state) => state.userCount);
+  const setChat = useStore ((state) => state.setChat)
+  const socket = useContext(Context);
 
-export default JoinChat
+	const joinChat = async () => {
+		if ("geolocation" in navigator) {
+			await navigator.geolocation.getCurrentPosition(async (pos) => {
+				const location = {
+					x: pos.coords.longitude,
+					y: pos.coords.latitude,
+				};
+				await socket.emit("create_chat", location);
+				setChat(true);
+			});
+		} else {
+			alert("You location is not available");
+		}
+	};
+
+	return (
+		<div>
+			{userCount ? <p>People online {userCount}</p> : null}
+			<button onClick={joinChat}>Join chat</button>
+		</div>
+	);
+};
---
+++ client/src/pages/StartChatPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { JoinChat } from '../components/JoinChat'
+
+export const StartChatPage = () => {
+  return (
+    <JoinChat/>
+  )
+}
