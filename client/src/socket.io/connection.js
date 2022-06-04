95863c429dfd1aebcd1ce59b32417a72bd6fe2bb
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-04 16:28:32 +0300

    node environment bug fix

--- client/src/socket.io/connection.js
+++ client/src/socket.io/connection.js
@@ -1,5 +1,11 @@
 import io from "socket.io-client";
 
-const SERVER_URL = "http://localhost:3001/";
+let SERVER_URL = "";
+
+console.log("Node environment:", process.env.NODE_ENV)
+if (process.env.NODE_ENV !== "production"){
+    SERVER_URL = "http://localhost:3001/";
+}
+
 export const socket = io.connect(SERVER_URL);
 
